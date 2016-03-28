d1bd7bd4244bc88c8ff5dd232304ac716ba28675
Denis Erfurt <denis.erfurt@gmail.com>
2016-03-28 19:46:19 +0200

    change tar.gz name

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ meteor build --architecture=os.linux.x86_64 .build
 # docker-compose ps
 docker-compose stop web
 ssh -i ~/.ssh/id_memhub root@159.203.96.25 rm -fR /bundle/*
-scp -i ~/.ssh/id_memhub .build/web.tar.gz root@159.203.96.25:/bundle
+scp -i ~/.ssh/id_memhub .build/memhub.io.tar.gz root@159.203.96.25:/bundle
 # ~/docker-machine scp -r .build/dapphub.io.tar.gz $MACHINE_INSTANCE:/bundle
 docker-compose up -d web
 # ssh -i ~/.ssh/id_dapphub root@104.236.215.91 docker stop web
